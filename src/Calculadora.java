4c611c47cfde9a61f4e3cf2fe2a17d0fae0d78ec
wes1909 <wesleipereira721@gmail.com>
2024-05-08 18:45:25 -0300

    Construtores Criados

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Calculadora {
+public class Calculadora {
+
+    int num1;
+    int num2;
+    int resultado;
+
+//Construtores
+
+ public Calculadora(){
+
+ }
+
+public Calculadora (int num1, int num2, int resultado){
+
+     this.num1 = num1;
+    this.num2 = num2;
+    this.resultado = resultado;
+
+}
+
+
 }
